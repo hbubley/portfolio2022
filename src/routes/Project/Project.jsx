b1b06043f2eb467649d2f9a5b5943e08bd6ddc01
Haley Bubley <haleybubley@haleys-imac.lan>
2022-04-23 11:05:43 -0500

    progress

--- src/routes/Project/Project.jsx
+++ src/routes/Project/Project.jsx
@@ -5,18 +5,37 @@ import styles from './Project.module.scss'
 import Card from '../../components/Card/Card';
 const Project = ({ data }) => {
     const { id } = useParams();
-    const [project, setProject] = useState()
+    const [project, setProject] = useState();
+    const [imageInFocus, setImageInFocus] = useState();
+    const [images, setImages] = useState();
+
     useEffect(() => {
         const _project = data.find((d) => `${d.id}` === `${id}`)
-        setProject(_project)
-        console.log("🚀 ~ file: Project.jsx ~ line 8 ~ Project ~ project", project, data, id)
+        setProject(_project);
+        if (_project?.images) {
+            const _images = JSON.parse(_project.images)
+            if (_images.length) {
+                setImages(_images)
+                setImageInFocus(_images[0])
+            }
+        }
     }, [data, id])
     return (
         <div className={styles.container}>
             <p className={styles.headingText}>{project?.title}</p>
-            <div>
+            <div className={styles.descriptionContainer}>
+                <p className={styles.description}>{project?.description}</p>
             </div>
-        </div>
+            <div>
+                <img className={styles.imageInFocus} src={imageInFocus?.src} />
+                <p className={styles.imageInFocusCaption}>{imageInFocus?.caption}</p>
+            </div>
+            <div className={styles.imagesContainer}>
+                {images?.length > 0 && images.map((image) => {
+                    return <div className={styles.image} onClick={() => setImageInFocus(image)} style={{ backgroundImage: `url(${image.src})` }} ></div>
+                })}
+            </div>
+        </div >
     )
 }
 
