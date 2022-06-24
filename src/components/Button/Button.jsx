fc9cf44af9ebee69182c20c77a24277e734586b4
Haley Bubley <haleybubley@haleys-imac.lan>
2022-06-24 13:27:52 -0500

    netlify form

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,9 @@ import styles from './Button.module.scss'
 
 import React from 'react'
 
-const Button = ({text, variant, size, type}) => {
+const Button = ({text, variant, size, type, onChange}) => {
   return (
-    <button className={`${variant ? styles[variant]: styles.primary} ${size ? styles[size]: styles.medium}`} type={type}>
+    <button className={`${variant ? styles[variant]: styles.primary} ${size ? styles[size]: styles.medium}`} type={type} onChange={onChange}>
         {text}
     </button>
   )
--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Button from '../../components/Button/Button'
 import styles from './Contact.module.scss'
 
 const Contact = () => {
+  const navigate = useNavigate()
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  }
+
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", "text": "test" })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <p className={`${styles.headingText} ${styles.textLeft}`}>Let's connect</p>
-        <form className={styles.form} name="contact" method="POST" data-netlify="true">
+        <form onSubmit={(e) => handleSubmit(e)} className={styles.form} name="contact" id="contact" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
           <div className={styles.formGroup}>
             <input className={styles.formInput} name="name" id="name" placeholder='Full Name' />
             <label className={styles.formLabel} for="name">First Name</label>
