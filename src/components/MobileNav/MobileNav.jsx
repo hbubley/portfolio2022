61061b374c7422a7c432990240c11ba7582dc1a1
Haley Bubley <haleybubley@haleys-imac.lan>
2022-06-24 09:32:46 -0500

    mobile nav progress :)

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -1,14 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styles from './MobileNav.module.scss'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { Link, useNavigate } from 'react-router-dom'
 
 const MobileNav = () => {
+    let navigate = useNavigate()
     const [open, setOpen] = useState(false);
+    const onNavigate = (to) => {
+        setOpen(false)
+        navigate(to, {replace: true})
+    }
     return (
-        <div className={styles.container}>
-            <FontAwesomeIcon icon={faBars} className={styles.icon} />
-        </div>
+        <>
+            <div className={styles.container}>
+                <FontAwesomeIcon icon={faBars} className={styles.icon} onClick={() => setOpen(!open)} />
+            </div>
+            {
+                open && <div className={styles.menu} style={{position: 'fixed', right: '0', top: "7%", backgroundColor: "white", width: '100%', fontSize: '4rem', zIndex: '11'}}>
+                    <Link to="/" onClick={() => onNavigate("/")}>Home</Link>
+                    <Link to="/about" onClick={() => onNavigate("/about")}>About</Link>
+                    <Link to="/portfolio" onClick={() => onNavigate("/portfolio")}>Portfolio</Link>
+                    <Link to="/contact" onClick={() => onNavigate("/contact")}>Contact</Link>
+                </div>
+            }
+        </>
     )
 }
 
--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Button from '../../components/Button/Button'
 import styles from './Contact.module.scss'
 
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <p className={styles.headingText}>Let's connect</p>
+      <p className={`${styles.headingText} ${styles.textLeft}`}>Let's connect</p>
+      <form className={styles.form}>
+        <div className={styles.formGroup}>
+          <input className={styles.formInput} name="name" id="name" placeholder='Full Name' />
+          <label className={styles.formLabel} for="name">First Name</label>
+        </div>
+        <div className={styles.formGroup}>
+          <input className={styles.formInput} name="email" id="email" placeholder='Email Address' />
+          <label className={styles.formLabel} for="email">Email Address</label>
+        </div>
+        <div className={styles.formGroup}>
+          <textarea className={styles.formInput} name="message" id="message" placeholder='Message' />
+          <label className={styles.formLabel} for="message">Message</label>
+        </div>
+        <Button text={"Submit"} />
+      </form>
     </div>
   )
 }
