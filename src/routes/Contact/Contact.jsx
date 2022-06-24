4085affbd4902028e681975c5a488f10b4d1f0cc
Haley Bubley <haleybubley@haleys-imac.lan>
2022-06-24 10:37:27 -0500

    adjust contact

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -5,22 +5,25 @@ import styles from './Contact.module.scss'
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <p className={`${styles.headingText} ${styles.textLeft}`}>Let's connect</p>
-      <form className={styles.form}>
-        <div className={styles.formGroup}>
-          <input className={styles.formInput} name="name" id="name" placeholder='Full Name' />
-          <label className={styles.formLabel} for="name">First Name</label>
-        </div>
-        <div className={styles.formGroup}>
-          <input className={styles.formInput} name="email" id="email" placeholder='Email Address' />
-          <label className={styles.formLabel} for="email">Email Address</label>
-        </div>
-        <div className={styles.formGroup}>
-          <textarea className={styles.formInput} name="message" id="message" placeholder='Message' />
-          <label className={styles.formLabel} for="message">Message</label>
-        </div>
-        <Button text={"Submit"} />
-      </form>
+      <div className={styles.formContainer}>
+        <p className={`${styles.headingText} ${styles.textLeft}`}>Let's connect</p>
+        <form className={styles.form}>
+          <div className={styles.formGroup}>
+            <input className={styles.formInput} name="name" id="name" placeholder='Full Name' />
+            <label className={styles.formLabel} for="name">First Name</label>
+          </div>
+          <div className={styles.formGroup}>
+            <input className={styles.formInput} name="email" id="email" placeholder='Email Address' />
+            <label className={styles.formLabel} for="email">Email Address</label>
+          </div>
+          <div className={styles.formGroup}>
+            <textarea className={styles.formInput} name="message" id="message" placeholder='Message' />
+            <label className={styles.formLabel} for="message">Message</label>
+          </div>
+          <Button text={"Submit"} />
+        </form>
+      </div>
+      <div className={styles.imageContainer}></div>
     </div>
   )
 }
